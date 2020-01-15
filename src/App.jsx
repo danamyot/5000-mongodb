fbcd2a2ce5db21b7f68a691f869b8eac6dfc2031
Dan Amyot <amyot.dan@gmail.com>
2020-01-14 20:16:00 -0500

    chore: code formatting and more consts

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,51 @@
-import React, { Component } from 'react' // 1
-import Content from './Content.jsx' // 1
-class App extends Component { // 1
-    constructor() { // 2
-        super() // 2
-        this.state = { // 2
-            usernameInput: "", // 2
-            passwordInput: "", // 2
-            username: undefined // 2 
-        } // 2
-    } // 2
-    usernameChange = evt => { // 4
-        this.setState({ usernameInput: evt.target.value }) // 4
-    } // 4
-    passwordChange = evt => { // 5
-        this.setState({ passwordInput: evt.target.value }) // 5
-    } // 5
-    submitHandler = async evt => { // 6
-        evt.preventDefault() // 6
-        console.log("username", this.state.username) // 6
-        console.log("password", this.state.passwordInput) // 6
-        let name = this.state.usernameInput // 6
-        let data = new FormData() // 7
-        data.append("username", name) // 7
-        data.append("password", this.state.passwordInput) // 7
-        let response = await fetch('/login', { method: "POST", body: data }) // 8
-        let body = await response.text() // 8
-        console.log("/login response", body) // 8
-        body = JSON.parse(body) // 8
-        if (body.success) { // 9
-            this.setState({ username: name }) // 9
-        } // 9
-    } // 6
-    render = () => { // 3
-        if (this.state.username === undefined) { // 3
-            return (<form onSubmit={this.submitHandler}> {/* 3 */}
-                Username <input type="text" onChange={this.usernameChange} />  {/* 4 */}
-                Password <input type="text" onChange={this.passwordChange} /> {/* 4 */}
-                <input type="submit" value="login" /> {/* 3 */}
-            </form>) // 3
-        } // 3
-        return (<Content />) // 3
+import React, { Component } from "react";
+import Content from "./Content.jsx";
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      usernameInput: "",
+      passwordInput: "",
+      username: undefined
+    };
+  }
+  usernameChange = evt => {
+    this.setState({ usernameInput: evt.target.value });
+  };
+  passwordChange = evt => {
+    this.setState({ passwordInput: evt.target.value });
+  };
+  submitHandler = async evt => {
+    evt.preventDefault();
+    console.log("username", this.state.username);
+    console.log("password", this.state.passwordInput);
+    let name = this.state.usernameInput;
+    let data = new FormData();
+    data.append("username", name);
+    data.append("password", this.state.passwordInput);
+    let response = await fetch("/login", { method: "POST", body: data });
+    let body = await response.text();
+    console.log("/login response", body);
+    body = JSON.parse(body);
+    if (body.success) {
+      this.setState({ username: name });
     }
+  };
+  render = () => {
+    if (this.state.username === undefined) {
+      return (
+        <form onSubmit={this.submitHandler}>
+          {" "}
+          Username <input
+            type="text"
+            onChange={this.usernameChange}
+          /> Password <input type="text" onChange={this.passwordChange} />{" "}
+          <input type="submit" value="login" />
+        </form>
+      );
+    }
+    return <Content />;
+  };
 }
 
-export default App // 1
-
-/* meta
-  ({
-    text:
-    {
-      1: `The App component is the top level component of this application.
-      We are importing the Content component, which will actually display the content.
-      The App component is mainly concerned with login.`,
-      2: `The state will store the usernameInput and passwordInput, which represent what
-      the user will enter in the login form, as well as the actual username when login
-      is successful`,
-      3: `In the render, we check if the user has signed in. If they haven't, we present them
-      with a sign in form. If they have signed in, we show them the content.`,
-      4: `This is the method used to update the usernameInput property of the state.`,
-      5: `This is the method used to update the passwordInput property of the state.`,
-
-    }
-
-  })
-  */
+export default App;
--- src/Content.jsx
+++ src/Content.jsx
@@ -1,26 +1,30 @@
-import React, { Component } from 'react' 
-import Post from './Post.jsx' 
-import NewPost from './NewPost.jsx' 
-class Content extends Component { 
-    constructor(props) { 
-        super(props) 
-        this.state = { 
-            posts: [] 
-        } 
-    } 
-    reload = async () => { 
-        let response = await fetch('/all-posts') 
-        let body = await response.text() 
-        console.log("/all-posts response", body) 
-        body = JSON.parse(body) 
-        this.setState({ posts: body }) 
-    } 
-    render = () => { 
-        return (<div> 
-            <button onClick={this.reload}> load </button> 
-            <div>{this.state.posts.map(p => <Post key={p._id} contents={p} />)}</div> 
-            <NewPost /> 
-        </div>) 
-    } 
-} 
-export default Content 
+import React, { Component } from "react";
+import Post from "./Post.jsx";
+import NewPost from "./NewPost.jsx";
+class Content extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+  }
+  reload = async () => {
+    const allPosts = (await fetch("/all-posts")).json();
+    console.log("/all-posts response", allPosts);
+    this.setState({ posts: allPosts });
+  };
+  render = () => {
+    return (
+      <div>
+        <button onClick={this.reload}> load </button>
+        <div>
+          {this.state.posts.map(p => (
+            <Post key={p._id} contents={p} />
+          ))}
+        </div>
+        <NewPost />
+      </div>
+    );
+  };
+}
+export default Content;
